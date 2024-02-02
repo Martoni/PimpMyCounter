02c2637ddc16da733b8b44f450c967060251722e
Fabien Marteau <fabien.marteau@armadeus.com>
2024-02-02 22:39:25 +0100

    not working pg count version

--- src/main/scala/Blink.scala
+++ src/main/scala/Blink.scala
@@ -8,6 +8,7 @@ import chisel3.util.log2Ceil
 object CounterTypes extends Enumeration {
   val NaturalCount = Value
   val FullAdderCount = Value
+  val FullPGAdderCount = Value
   val PdChain = Value
 }
 
@@ -34,6 +35,11 @@ class Blink(val COUNT_WIDTH: Int = 32,
       val counter = Module(new FullAdderCount(COUNT_WIDTH))
       io.leds := counter.io.count(COUNT_WIDTH-1, COUNT_WIDTH-LED_WIDTH)
     }
+    case CounterTypes.FullPGAdderCount => {
+      println("Generate Full PG Adder Count of " + COUNT_WIDTH + " bits")
+      val counter = Module(new FullPGAdderCount(COUNT_WIDTH, usePG = true))
+      io.leds := counter.io.count(COUNT_WIDTH-1, COUNT_WIDTH-LED_WIDTH)
+    }
   }
 }
 
@@ -49,19 +55,75 @@ class NaturalCount(val COUNT_WIDTH: Int = 32) extends Module {
 }
 
 /* FullAdder counter */
-class FullAdderCount(val COUNT_WIDTH: Int = 32) extends Module {
+class FullAdderCount(val COUNT_WIDTH: Int = 44, val usePG: Boolean := false) extends Module {
   val io = IO(new Bundle {
     val count = Output(UInt(COUNT_WIDTH.W))
   })
 
   val counterValue = RegInit(0.U(COUNT_WIDTH.W))
-  val addition = Module(new FullAdderAddition(COUNT_WIDTH))
+  val addition =  if (usePG == true)
+    Module(new FullPGAdderAddition(COUNT_WIDTH))
+  else
+    Module(new FullAdderAddition(COUNT_WIDTH))
   addition.io.a := counterValue
   addition.io.b := 1.U
   counterValue := addition.io.s
 
   io.count := counterValue
 }
+
+
+class FullPGAdderAddition(val COUNT_WIDTH: Int = 44) extends Module {
+  val io = IO(new Bundle {
+    val a   = Input(UInt(COUNT_WIDTH.W))
+    val b   = Input(UInt(COUNT_WIDTH.W))
+    val c   = Output(Bool())
+    val s   = Output(UInt(COUNT_WIDTH.W))
+  })
+
+  val s_vec = Wire(Vec(COUNT_WIDTH, Bool()))
+
+  /* Carry-LookAhead module */
+  val cla_mod = Module(new CarryLookAhead(COUNT_WIDTH))
+  cla_mod.io.c0 := false.B
+
+  val fullPGAdders = for(i <- 0 until COUNT_WIDTH) yield {
+    val full_pg_adder = Module(new FullPGAdder())
+    full_pg_adder.io.a := io.a(i)
+    full_pg_adder.io.b := io.b(i)
+    if (i == 0) {
+      full_adder.io.cin := false.B
+    } else {
+      full_adder.io.cin := cla_mod.io.c(i-1)
+    }
+    cla_mod.io.p(i) := full_pg_adder.io.p
+    cla_mod.io.g(i) := full_pg_adder.io.g
+    s_vec(i) := full_adder.io.s
+    full_adder
+  }
+
+  io.c := cla_mod.io.c(COUNT_WIDTH-2)
+  io.s := s_vec.asUInt
+}
+
+
+class CarryLookAhead(val COUNT_WIDTH: Int = 44) extends Module {
+  val io = IO(new Bundle {
+    val p =  Input(Vec(COUNT_WIDTH.W), Bool())
+    val g =  Input(Vec(COUNT_WIDTH.W), Bool())
+    val c = Output(Vec((COUNT_WIDTH - 1).W), Bool())
+    val c0= Input(Bool())
+  })
+
+  for (i <- 0 until COUNT_WIDTH) {
+    if (i == 0) {
+      io.c(i+1) = io.g(i) | (io.p(i) & io.c0)
+    } else {
+      io.c(i+1) = io.g(i) | (io.p(i) & io.c(i-1))
+    }
+  }
+}
+
 
 /* Full Adder additionner */
 class FullAdderAddition(val COUNT_WIDTH: Int = 32) extends Module {
@@ -101,6 +163,31 @@ object Blink extends App {
 //  (new chisel3.stage.ChiselStage).emitVerilog(new Blink(COUNT_WIDTH=128), args)
   (new chisel3.stage.ChiselStage).emitVerilog(new Blink(), args)
 }
+
+
+class FullPGAdder extends Module {
+  val io = IO(new Bundle {
+    val a    = Input(Bool())
+    val b    = Input(Bool())
+    val cin  = Input(Bool())
+    val s    = Output(Bool())
+    val p    = Output(Bool())
+    val g    = Output(Bool())
+  })
+
+  val ha0 = Module(new HalfAdder())
+  val ha1 = Module(new HalfAdder())
+
+  ha0.io.a := io.a
+  ha0.io.b := io.b
+  ha1.io.a := ha0.io.s
+  ha1.io.b := io.cin
+
+  io.s := ha1.io.s
+  io.p := ha0.io.s
+  io.g := ha0.io.c
+}
+
 
 class FullAdder extends Module {
   val io = IO(new Bundle {
